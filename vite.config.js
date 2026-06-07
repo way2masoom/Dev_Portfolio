13647ecf06390f63b389aea1c25567b11e8486e3
MD ALAM <alkaserphone@MacBook-Air.local>
2026-06-07 23:56:35 +0545

    added a path alias for easir import

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// Added a path alias for easier imports in the project
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Create __dirname manually
+const __filename = fileURLToPath(import.meta.url) // Get the current file's absolute path
+const __dirname = path.dirname(__filename) // Get the current directory path
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  // Configure path aliases
+  resolve: {
+    alias: {
+      // Allows importing files using "@"
+      // Example:
+      // import Hero from '@/components/Hero'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
