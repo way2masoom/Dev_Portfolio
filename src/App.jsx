2ecdd0e9484a64db167a42468a7501ea189f1980
MD ALAM <alkaserphone@MacBook-Air.local>
2026-06-07 23:57:03 +0545

    Portfolio project basic sectup is done

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Navbar } from "@/Layout/Navbar"
+import { Hero } from "@/sections/Hero"
+import { About } from "@/sections/About"
+import { Projects } from "@/sections/Projects"
+import { Experience  } from "@/sections/Experience"
+import { Testimonials } from "@/sections/Testimonials"
+import { Contact } from "@/sections/Contact"
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact/>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
